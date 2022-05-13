b12971b96934cdc240b7922e367d0afd4b67e608
kiwialec <alec.bertram@disciplemedia.com>
2022-05-13 09:47:47 +0100

    create

--- create_link.php
+++ create_link.php
@@ -2,12 +2,18 @@
 require_once("lib/encrypt.php");
 
 if(isset($_POST["submittedForm"])){
+    $allowedVisibilityTypes = [];
+    if($_POST["visibilitySecret"] == "on") $allowedVisibilityTypes[] = "secret";
+    if($_POST["visibilityPrivate"] == "on") $allowedVisibilityTypes[] = "private";
+    if($_POST["visibilityPublic"] == "on") $allowedVisibilityTypes[] = "public";
+    if($_POST["visibilityMandatory"] == "on") $allowedVisibilityTypes[] = "mandatory";
+
     $display_link = "https://disciple-membercreated.herokuapp.com/?_mc=" . encrypt_and_sign(json_encode([
         "apiHost" => $_POST["apiHost"],
         "apiKey" => $_POST["apiKey"],
         "authenticatedLinkSecret" => $_POST["authenticatedLinkSecret"],
-        "createdAt" => time()
-        
+        "createdAt" => time(),
+        "allowed_visibility" => $allowedVisibilityTypes
     ]), hex2bin(getenv("secretEncryptKey")));
 }
 
@@ -19,5 +25,10 @@ if(isset($display_link)){
     Community URL: <input type=text name="apiHost" placeholder="https://my-community.disciplemedia.com" ><br>
     API Key: <input type=text name="apiKey" placeholder="abcdefg123" ><br>
     Authenticated Links Shared Secret: <input type=text name="authenticatedLinkSecret" placeholder="abcdefg123" ><br>
+    <strong>Allowed Group Types</strong><br>
+    <input type=checkbox name="visibilitySecret" checked> Secret<br>
+    <input type=checkbox name="visibilityPrivate" checked> Private<br>
+    <input type=checkbox name="visibilityPublic"> Public<br>
+    <input type=checkbox name="visibilityMandatory"> Mandatory<br>
     <input type="submit" name="submittedForm" value="Create Link">
 </form>
